8bd64aa1b17d8496d30a4675a5496ede75a4d8ce
Boehrsi <git@boehrsi.de>
2025-10-04 10:27:28 +0200

    build: recreate example Android code
    Signed-off-by: Boehrsi <git@boehrsi.de>

--- example/android/app/src/main/kotlin/com/example/on_upgrade_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/on_upgrade_example/MainActivity.kt
@@ -1,5 +1,5 @@
-package de.boehrsi.on_upgrade_example
+package com.example.on_upgrade_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity()
